bbee4a584711c3197bfe59f58b7ef903d7018ce6
Jackson Ming Hu <huming2207@gmail.com>
2018-03-10 15:30:29 +1100

    DicePair: implement sub class

--- src/model/DicePairImpl.java
+++ src/model/DicePairImpl.java
@@ -1,24 +1,36 @@
 package model;
 
 import model.interfaces.DicePair;
+import model.interfaces.GameEngine;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DicePairImpl implements DicePair
 {
     @Override
     public int getDice1()
     {
-        return 0;
+        // Generate a random integer between 1 to 6
+        // It seems to be the best way to generate any random number with range in Java is using "ThreadLocalRandom"...
+        // Ref: https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
+        return ThreadLocalRandom.current().nextInt(1, GameEngine.NUM_FACES + 1);
     }
 
     @Override
     public int getDice2()
     {
-        return 0;
+        return ThreadLocalRandom.current().nextInt(1, GameEngine.NUM_FACES + 1);
     }
 
     @Override
     public int getNumFaces()
     {
-        return 0;
+        return GameEngine.NUM_FACES;
+    }
+
+    @Override
+    public String toString()
+    {
+        return null;
     }
 }
