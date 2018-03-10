72b6e8ae8ed4938f7d3e01a8b007346d6fd31677
Jackson Ming Hu <huming2207@gmail.com>
2018-03-10 19:54:15 +1100

    GameEngine: implement private members

--- src/model/GameEngineImpl.java
+++ src/model/GameEngineImpl.java
@@ -4,10 +4,20 @@ import model.interfaces.GameEngine;
 import model.interfaces.GameEngineCallback;
 import model.interfaces.Player;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class GameEngineImpl implements GameEngine
 {
+    private Collection<Player> playerList;
+    private Collection<GameEngineCallback> callbackList;
+
+    public GameEngineImpl()
+    {
+        this.playerList = new ArrayList<>();
+        this.callbackList = new ArrayList<>();
+    }
+
     @Override
     public boolean placeBet(Player player, int bet)
     {
@@ -29,36 +39,40 @@ public class GameEngineImpl implements GameEngine
     @Override
     public void addPlayer(Player player)
     {
-
+        playerList.add(player);
     }
 
     @Override
     public Player getPlayer(String id)
     {
+        for(Player player : playerList) {
+            if(player.getPlayerId().equals(id)) return player;
+        }
+
         return null;
     }
 
     @Override
     public boolean removePlayer(Player player)
     {
-        return false;
+        return playerList.remove(player);
     }
 
     @Override
     public void addGameEngineCallback(GameEngineCallback gameEngineCallback)
     {
-
+        callbackList.add(gameEngineCallback);
     }
 
     @Override
     public boolean removeGameEngineCallback(GameEngineCallback gameEngineCallback)
     {
-        return false;
+        return callbackList.remove(gameEngineCallback);
     }
 
     @Override
     public Collection<Player> getAllPlayers()
     {
-        return null;
+        return playerList;
     }
 }
