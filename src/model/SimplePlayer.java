b91d02907d858acd6851fb27c54c56fc0c29c925
Jackson Ming Hu <huming2207@gmail.com>
2018-03-10 13:14:41 +1100

    SimplePlayer: implement private members

--- src/model/SimplePlayer.java
+++ src/model/SimplePlayer.java
@@ -5,34 +5,48 @@ import model.interfaces.Player;
 
 public class SimplePlayer implements Player
 {
+    private String playerId;
+    private String playerName;
+    private int bet;
+    private int points;
+    private DicePair dicePair;
+
+    public SimplePlayer(String playerId, String playerName, int bet)
+    {
+        this.playerId = playerId;
+        this.playerName = playerName;
+        this.bet = bet;
+        this.dicePair = new DicePairImpl();
+    }
+
     @Override
     public String getPlayerName()
     {
-        return null;
+        return playerName;
     }
 
     @Override
     public void setPlayerName(String playerName)
     {
-
+        this.playerName = playerName;
     }
 
     @Override
     public int getPoints()
     {
-        return 0;
+        return points;
     }
 
     @Override
     public void setPoints(int points)
     {
-
+        this.points = points;
     }
 
     @Override
     public String getPlayerId()
     {
-        return null;
+        return playerId;
     }
 
     @Override
@@ -50,12 +64,12 @@ public class SimplePlayer implements Player
     @Override
     public DicePair getRollResult()
     {
-        return null;
+        return dicePair;
     }
 
     @Override
     public void setRollResult(DicePair rollResult)
     {
-
+        this.dicePair = rollResult;
     }
 }
