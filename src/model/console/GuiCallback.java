15be5ec773276200f0bc8a0662ebc993dc157ea3
Jackson Ming Hu <huming2207@gmail.com>
2018-05-02 14:16:21 +1000

    GuiCallback: init

---
+++ src/model/console/GuiCallback.java
@@ -0,0 +1,33 @@
+package model.console;
+
+import model.interfaces.DicePair;
+import model.interfaces.GameEngine;
+import model.interfaces.GameEngineCallback;
+import model.interfaces.Player;
+
+public class GuiCallback implements GameEngineCallback
+{
+    @Override
+    public void intermediateResult(Player player, DicePair dicePair, GameEngine gameEngine)
+    {
+
+    }
+
+    @Override
+    public void result(Player player, DicePair result, GameEngine gameEngine)
+    {
+
+    }
+
+    @Override
+    public void intermediateHouseResult(DicePair dicePair, GameEngine gameEngine)
+    {
+
+    }
+
+    @Override
+    public void houseResult(DicePair result, GameEngine gameEngine)
+    {
+
+    }
+}
