5388732f43fb15486ad6d9b27645de846d72cd9f
Jackson Ming Hu <huming2207@gmail.com>
2018-05-03 18:44:53 +1000

    InfoPanel: place label and content separately

--- src/views/panel/InfoPanel.java
+++ src/views/panel/InfoPanel.java
@@ -7,34 +7,49 @@ import java.awt.*;
 
 public class InfoPanel extends JPanel
 {
-    private JLabel playerLabel = new JLabel("Player: Unknown");
-    private JLabel playerBalanceLabel = new JLabel("Balance: Unknown");
-    private JLabel playerBetLabel = new JLabel("Bet: Unknown");
+    private JLabel playerLabel = new JLabel("Unknown");
+    private JLabel playerBalanceLabel = new JLabel("Unknown");
+    private JLabel playerBetLabel = new JLabel("Unknown");
     private JLabel playerResultLabel = new JLabel("Result: Unknown");
-    private JLabel houseLabel = new JLabel("House");
-    private JLabel houseBetLabel = new JLabel("Bet: Unknown");
+    private JLabel houseLabel = new JLabel("Unknown");
+    private JLabel houseBetLabel = new JLabel("Unknown");
     private JLabel houseResultLabel = new JLabel("Result: Unknown");
 
     private GameController gameController;
 
     public InfoPanel(GameController gameController)
     {
-        super(new GridLayout(0, 1));
+        super(new GridLayout(0, 2));
 
         // Enlarge title labels, i.e. player label and house label
-        this.playerLabel.setFont(new Font("Serif", Font.BOLD, 25));
-        this.houseLabel.setFont(new Font("Serif", Font.BOLD, 25));
+        JLabel playerTitleLabel = new JLabel("Player");
+        JLabel houseTitleLabel = new JLabel("House");
+        playerTitleLabel.setFont(new Font("Serif", Font.BOLD, 25));
+        houseTitleLabel.setFont(new Font("Serif", Font.BOLD, 25));
 
         // Set game controller
         this.gameController = gameController;
 
         // Add all components in
+        this.add(playerTitleLabel);
         this.add(this.playerLabel);
+
+        this.add(new JLabel("Balance: "));
         this.add(this.playerBalanceLabel);
+
+        this.add(new JLabel("Bet: "));
         this.add(this.playerBetLabel);
+
+        this.add(new JLabel("Result: "));
         this.add(this.playerResultLabel);
+
+        this.add(houseTitleLabel);
         this.add(this.houseLabel);
+
+        this.add(new JLabel("Bet: "));
         this.add(this.houseBetLabel);
+
+        this.add(new JLabel("Balance: "));
         this.add(this.houseResultLabel);
     }
 }
